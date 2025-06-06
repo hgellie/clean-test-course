7c5e2afc52e1a5b4e6282a01282a5930ae9c34c7
hgellie <heidigellersen@yahoo.com>
2025-06-06 16:21:44 -0500

    Adding frontend testing

--- frontend/src/pages/Order/Order.test.js
+++ frontend/src/pages/Order/Order.test.js
@@ -37,7 +37,8 @@ describe('Test Order', () => {
     );
     //Assert: replace the return true.
     await waitFor(() => {
-      return true;
+      expect(screen.getByText('$2.50'))
+        .toBeInTheDocument();
     });
   });
 
@@ -63,7 +64,8 @@ describe('Test Order', () => {
     );
     //Assert: replace the return true.
     await waitFor(() => {
-      return true;
+      expect(screen.getByText('$5.00'))
+        .toBeInTheDocument();
     });
   });
 });
